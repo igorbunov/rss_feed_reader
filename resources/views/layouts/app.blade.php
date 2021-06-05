09253d06e3094b62bf2c7d5b1e887b7847af8e33
igorbunov <igorbunov.ua@gmail.com>
2021-06-05 21:01:28 +0300

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="/favicon.ico" style="width: 30px; height: 30px;"/>
+                    <span style="vertical-align: text-top;font-weight: bold;font-size: 20px;margin-left: 5px;">{{ config('app.name', 'Laravel') }}</span>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
