4550c7a44625d3d3414c5b94e921348d007932de
igorbunov <igorbunov.ua@gmail.com>
2021-06-15 21:29:24 +0300

    add no interaction to migration on prod

--- app/Jobs/DeployCodeJob.php
+++ app/Jobs/DeployCodeJob.php
@@ -34,7 +34,7 @@ class DeployCodeJob implements ShouldQueue
             'git pull',
             'composer install',
             'npm install',
-            'php artisan migrate --force'
+            'php artisan migrate --force --no-interaction'
         ];
 
         exec(implode(' & ', $deployCommands));
